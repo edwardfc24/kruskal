8312373d09c39e52f1876a518ab8a44b8242e26d
Eduardo  Flores Coppede <eduardofc@gmail.com>
2020-03-28 11:34:15 -0400

    Primera fución básica para orden de los datos o nodos

--- Kruskal.py
+++ Kruskal.py
@@ -1,5 +1,9 @@
 class Kruskal:
 
     def __init__(self):
-        self.nodes = []
-        self.order = []
+        self.nodes = {}
+        self.order = {}
+
+    def validate_data(self, node):
+        self.nodes[node] = node
+        self.order[node] = 0
