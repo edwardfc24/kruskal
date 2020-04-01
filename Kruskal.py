793f41a2fcee8c93fd092872be9c9c5e3aa08b48
Eduardo  Flores Coppede <eduardofc@gmail.com>
2020-03-31 21:20:59 -0400

    Kruskal David Chavez Completo

--- Kruskal.py
+++ Kruskal.py
@@ -1,13 +1,59 @@
 class Kruskal:
+    # Método Kruskal(Grafo)
+    # Se inicializa el árbol de expansión mínima vacío                                                 
+    # Se inicializa una estructura de unión-búsqueda                                                    
+    # Se ordenan las aristas de menor a mayor peso                                                     
+    #         Para cada arista a que une 2 vértices (u, v)
+    #     Si u y v no están en la misma componente
+    #             Se añade la arista a al árbol de expansión mínima. 
+    #                         Se unen  las componentes de u y v
+    #                 Fin Si
+    # Fin Para
+    # Fin Método Kruskal
 
-# Comentario Inicial
+
     def __init__(self):
         self.nodes = {}
         self.order = {}
-        
-    #comentario 1
 
-    def validate_data(self, node):
+
+    def prepare_data(self, node):
         self.nodes[node] = node
         self.order[node] = 0
 
+
+    def find_node(self, node):
+        if self.nodes[node] != node:
+            self.nodes[node] = self.find_node(self.nodes[node])
+        return self.nodes[node]
+
+
+    def validate_union(self, origin, destination):
+        origin_auxiliar = self.find_node(origin)
+        destination_auxiliar = self.find_node(destination)
+        if origin_auxiliar != destination_auxiliar:
+            if self.order[origin_auxiliar] > self.order[destination_auxiliar]:
+                self.nodes[destination_auxiliar] = origin_auxiliar
+            else:
+                self.nodes[origin_auxiliar] = destination_auxiliar
+                if self.order[origin_auxiliar] == self.order[destination_auxiliar]:
+                    self.order[destination_auxiliar] += 1
+
+
+    def kruskal(self, nodes, edges):
+        # nodes ['a', 'b', 'c'.....]
+        # edges ['origen', 'destino', entero]
+        tree = []
+        for node in nodes: 
+            self.prepare_data(node)
+        # Ordenar la lista
+        edges.sort()
+        for edge in edges:
+            origin, destination, weight = edge
+            #  String origin = edge[0];
+            #  String destination = edge[1];
+            #  int weigth = edge[2];
+            if self.find_node(origin) != self.find_node(destination):
+                self.validate_union(origin, destination)
+                tree.append(edge)
+        return tree
