16d7c8a56963fedb7986faeb1a48d9465c32399d
David <papapote3@gmail.com>
2020-04-03 20:59:40 -0400

    KruskalDavid Completo
    Es mi cambio

--- Kruskal.py
+++ Kruskal.py
@@ -1,32 +1,28 @@
 class Kruskal:
     # Método Kruskal(Grafo)
-    # Se inicializa el árbol de expansión mínima vacío                                                 
-    # Se inicializa una estructura de unión-búsqueda                                                    
-    # Se ordenan las aristas de menor a mayor peso                                                     
+    # Se inicializa el árbol de expansión mínima vacío
+    # Se inicializa una estructura de unión-búsqueda
+    # Se ordenan las aristas de menor a mayor peso
     #         Para cada arista a que une 2 vértices (u, v)
     #     Si u y v no están en la misma componente
-    #             Se añade la arista a al árbol de expansión mínima. 
+    #             Se añade la arista a al árbol de expansión mínima.
     #                         Se unen  las componentes de u y v
     #                 Fin Si
     # Fin Para
     # Fin Método Kruskal
 
-
     def __init__(self):
         self.nodes = {}
         self.order = {}
-
 
     def prepare_data(self, node):
         self.nodes[node] = node
         self.order[node] = 0
 
-
     def find_node(self, node):
         if self.nodes[node] != node:
             self.nodes[node] = self.find_node(self.nodes[node])
         return self.nodes[node]
-
 
     def validate_union(self, origin, destination):
         origin_auxiliar = self.find_node(origin)
@@ -39,12 +35,11 @@ class Kruskal:
                 if self.order[origin_auxiliar] == self.order[destination_auxiliar]:
                     self.order[destination_auxiliar] += 1
 
-
     def kruskal(self, nodes, edges):
         # nodes ['a', 'b', 'c'.....]
         # edges ['origen', 'destino', entero]
         tree = []
-        for node in nodes: 
+        for node in nodes:
             self.prepare_data(node)
         # Ordenar la lista
         edges.sort()
@@ -57,3 +52,30 @@ class Kruskal:
                 self.validate_union(origin, destination)
                 tree.append(edge)
         return tree
+
+    def RUN(self):
+        print("Ingrese los nodos para Meterlo: ")
+        nodos = input()
+        lista_nodos = nodos.split(',')
+        print('Ingrese el origen, destino y peso separado por una coma'
+              'Coloque 0 para finalizar la entrada de datos')
+        Valor = ''
+        lista_arista = []
+
+        while Valor.__ne__('0'):
+            Valor = input()
+
+            if Valor.__eq__('0'):
+                break
+
+            valor2 = Valor.split(',')
+            nuevo = int(valor2[2])
+            valor2[2] = nuevo
+            lista_arista.append(valor2)
+
+        print(self.kruskal(lista_nodos, lista_arista))
+
+Objeto = Kruskal()
+Objeto.RUN()
+
+# lo estoy corriendo por phy.charm.
