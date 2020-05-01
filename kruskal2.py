524d5e4dee1195fb6210314818cb75daa51b65a9
luis julio alvarez <luisjac98@outlook.com>
2020-05-01 02:25:02 -0300

    primera funcion basica

--- kruskal2.py
+++ kruskal2.py
@@ -1,5 +1,9 @@
 class Kruskal:
     
     def __init__(self):
-        self.nodes=[]
-        self.order=[]
+        self.nodes={}
+        self.order={}
+
+    def validatedata(self,node):
+        self.nodes[node]=node
+        self.order[node]=0
